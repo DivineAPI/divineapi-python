195536de90518a8bdcf474d5a3e314110ec1f270
nitishautomates <nitishautomates@gmail.com>
2026-07-07 00:06:42 +0530

    Release v1.0.4

--- divineapi/__init__.py
+++ divineapi/__init__.py
@@ -29,7 +29,7 @@ from .numerology import NumerologyApi
 from .pdf import PdfReportApi
 from .western import WesternApi
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 __all__ = [
     "DivineApi",
